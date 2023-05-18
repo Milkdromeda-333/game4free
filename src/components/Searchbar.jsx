9c71cd0ad27caaa44a5be1b9728f4f4a02f40afe
Anjanique <anjaniquemackey@gmail.com>
2023-05-18 00:18:58 -0700

    style: search result

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import SearchResult from "./SearchResult";
 
 export default function Searchbar() {
 
-    const [userQuery, setUserQuery] = useState(null);
+    const [userQuery, setUserQuery] = useState("");
     const [liveGames, setLiveGames] = useState([]);
-    const [foundGames, setFoundGames] = useState([]);
+    const [foundGames, setFoundGames] = useState(null);
+
+    const myRef = useRef();
+    const itemContainer = useRef();
 
     function updateQuery(e) {
         const { value } = e.target;
@@ -14,7 +17,7 @@ export default function Searchbar() {
         searchGames(value);
     }
 
-    const resultsBoxesArr = foundGames.map(game => <SearchResult key={game.id} {...game} />)
+    const resultsBoxesArr = foundGames?.map(game => <SearchResult key={game.id} {...game} />);
 
     function searchGames(value) {
         if (value) {
@@ -25,13 +28,18 @@ export default function Searchbar() {
         }
     }
 
-    function resetQuery() {
-        setUserQuery("");
-        setFoundGames([]);
+    function resetQuery(e) {
+        console.log(e.target)
+        // DOCS checks if the target is anywhere but the results contianer
+        if (e.target.nextSibling.className === "searchbar__results-container") {
+            setUserQuery("");
+            setFoundGames([]);
+        }
+        
     }
 
     useEffect(() => {
-        if (userQuery === null) {
+        if (liveGames.length === 0) {
             const options = {
                 method: 'GET',
                 url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
@@ -47,14 +55,16 @@ export default function Searchbar() {
     }, []);
     
     return (
-        <form className="searchbar" onBlur={ resetQuery }>
+        <form className="searchbar">
             <input type="search"
                 onChange={updateQuery}
+                // onMouseLeave={resetQuery}
                 className="searchbar__searchbar"
                 value={userQuery}
+                ref={(myRef)}
                 placeholder="have a game in mind? search the name."
             />
-            <ul className="searchbar__results-container">{resultsBoxesArr}</ul>
+            {foundGames?.length > 0 && <ul className="searchbar__results-container" ref={itemContainer}>{resultsBoxesArr}</ul>}
         </form>
     )
 }
